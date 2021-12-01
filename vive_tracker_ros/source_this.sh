b24c56866dc2492fc2fd65790251b419384bf8b1
Kevin Jordil <31761059+KevinJordil@users.noreply.github.com>
2021-12-01 16:28:30 +0100

    Set correct usb id

--- vive_tracker_ros/source_this.sh
+++ vive_tracker_ros/source_this.sh
@@ -1,7 +1,7 @@
 # Add permission to access the tracker connected to the USB
 echo 'Adding permissions for USB Tracker access'
-vive_bus=`lsusb | grep 28de:2022 | awk -F" " {'print $2'}`
-vive_dev=`lsusb | grep 28de:2022 | awk -F" " {'print $4'} | awk -F":" {'print $1'}`
+vive_bus=`lsusb | grep 28de:2101 | awk -F" " {'print $2'}`
+vive_dev=`lsusb | grep 28de:2101 | awk -F" " {'print $4'} | awk -F":" {'print $1'}`
 if [ -z "$vive_bus" ]; then
   echo "Error! No VIVE tracker found. Connect tracker and try again"
 else
