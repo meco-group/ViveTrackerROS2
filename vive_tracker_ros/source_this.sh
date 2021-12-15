5a6d2b0c6c258290591b41d59f2839f442182f76
KevinJordil <kevin.jordil@heig-vd.ch>
2021-12-15 17:09:42 +0100

    Update script for multiple trackers

--- vive_tracker_ros/source_this.sh
+++ vive_tracker_ros/source_this.sh
@@ -1,11 +1,15 @@
 # Add permission to access the tracker connected to the USB
 echo 'Adding permissions for USB Tracker access'
-vive_bus=`lsusb | grep 28de:2101 | awk -F" " {'print $2'}`
-vive_dev=`lsusb | grep 28de:2101 | awk -F" " {'print $4'} | awk -F":" {'print $1'}`
+vive_bus=($(lsusb | grep 28de:2101 | awk -F" " {'print $2'}))
+vive_dev=($(lsusb | grep 28de:2101 | awk -F" " {'print $4'} | awk -F":" {'print $1'}))
 if [ -z "$vive_bus" ]; then
   echo "Error! No VIVE tracker found. Connect tracker and try again"
 else
-  `sudo chmod 666 /dev/bus/usb/${vive_bus}/${vive_dev}`
+  for i in "${!vive_dev[@]}"
+    do
+      echo "Add permission to /dev/bus/usb/${vive_bus[$i]}/${vive_dev[$i]}"
+      `sudo chmod 666 /dev/bus/usb/${vive_bus[$i]}/${vive_dev[$i]}`
+    done
 fi
 # To configure ROS environment
 echo 'Configuring environment variables for ROS...'
